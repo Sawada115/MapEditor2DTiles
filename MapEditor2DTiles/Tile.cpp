aaca3b5ee037c92fe9666cdf221e7f1df5a02f9b
TSITCL\s162031 <kouhei.u.1997@gmai.com>
2017-09-29 12:41:43 +0900

    タイル情報修正

--- MapEditor2DTiles/Tile.cpp
+++ MapEditor2DTiles/Tile.cpp
@@ -10,20 +10,21 @@
 
 // ----- タイルのベース --------------------------------------------------------------------------------
 TileBase::TileData TileBase::m_datas[80] = {	
-	{ false,L"None" },{ false,L"RoadA1" },{ false,L"RoadA2" },{ false,L"RoadA3" },{ true,L"RoadB1" },{ true,L"RoadB2" },{ true,L"RoadB3" },{ false,L"RoadC1" },{ false,L"RoadC2" },{ false,L"RoadC3" },
 									// -------  10  --------- //
-	{ false,L"Grass1" },{ false,L"RoadA4" },{ false,L"RoadA5" },{ false,L"RoadA6" },{ true,L"RoadB4" },{ true,L"RoadB5" },{ true,L"RoadB6" },{ false,L"RoadC4" },{ false,L"RoadC5" },{ false,L"RoadC6" },
+	{ false,L"None" },{ false,L"RoadA1" },{ false,L"RoadA2" },{ false,L"RoadA3" },{ true,L"RoadB1" },{ true,L"RoadB2" },{ true,L"RoadB3" },{ false,L"RoadC1" },{ false,L"RoadC2" },{ false,L"RoadC3" },
 									// -------  20  --------- //
-	{ false,L"Grass2" },{ false,L"RoadA7" },{ false,L"RoadA8" },{ false,L"RoadA9" },{ true,L"RoadB7" },	{ true,L"RoadB8" },{ true,L"RoadB9" },{ false,L"RoadC7" },{ false,L"RoadC8" },{ false,L"RoadC9" },
+	{ false,L"Grass1" },{ false,L"RoadA4" },{ false,L"RoadA5" },{ false,L"RoadA6" },{ true,L"RoadB4" },{ true,L"RoadB5" },{ true,L"RoadB6" },{ false,L"RoadC4" },{ false,L"RoadC5" },{ false,L"RoadC6" },
 									// -------  30  --------- //
-	{ false,L"Grass3" },{ true,L"RoadD1" },{ true,L"RoadD2" },{ true,L"RoadD3" },{ true,L"RoadE1" },{ true,L"RoadE2" },{ true,L"RoadE3" },{ true,L"RoadF1" },{ true,L"RoadF2" },{ true,L"RoadF3" },
+	{ false,L"Grass2" },{ false,L"RoadA7" },{ false,L"RoadA8" },{ false,L"RoadA9" },{ true,L"RoadB7" },	{ true,L"RoadB8" },{ true,L"RoadB9" },{ false,L"RoadC7" },{ false,L"RoadC8" },{ false,L"RoadC9" },
 									// -------  40  --------- //
-	{ false,L"Grass4" },{ true,L"RoadD4" },{ true,L"RoadD5" },{ true,L"RoadD6" },{ true,L"RoadE4" },{ true,L"RoadE5" },{ true,L"RoadE6" },{ true,L"RoadF4" },{ true,L"RoadF5" },{ true,L"RoadF6" },
+	{ false,L"Grass3" },{ true,L"RoadD1" },{ true,L"RoadD2" },{ true,L"RoadD3" },{ true,L"RoadE1" },{ true,L"RoadE2" },{ true,L"RoadE3" },{ true,L"RoadF1" },{ true,L"RoadF2" },{ true,L"RoadF3" },
 									// -------  50  --------- //
-	{ false,L"Grass1" },{ true,L"RoadD7" },{ true,L"RoadD8" },{ true,L"RoadD9" },{ true,L"RoadE7" },{ true,L"RoadE8" },{ true,L"RoadE9" },{ true,L"RoadF7" },{ true,L"RoadF8" },{ true,L"RoadF9" },
+	{ false,L"Grass4" },{ true,L"RoadD4" },{ true,L"RoadD5" },{ true,L"RoadD6" },{ true,L"RoadE4" },{ true,L"RoadE5" },{ true,L"RoadE6" },{ true,L"RoadF4" },{ true,L"RoadF5" },{ true,L"RoadF6" },
 									// -------  60  --------- //
-	{ true,L"Tree1" },{ true,L"Tree2" },{ false,L"Grass1" },{ false,L"Grass2" },{ false,L"Grass3" },{ false,L"Grass4" },{ false,L"Grass1" },{ false,L"Grass2" },{ false,L"Grass3" },{ false,L"Grass4" },
+	{ false,L"Grass1" },{ true,L"RoadD7" },{ true,L"RoadD8" },{ true,L"RoadD9" },{ true,L"RoadE7" },{ true,L"RoadE8" },{ true,L"RoadE9" },{ true,L"RoadF7" },{ true,L"RoadF8" },{ true,L"RoadF9" },
 									// -------  70  --------- //
+	{ true,L"Tree1" },{ true,L"Tree2" },{ false,L"Grass1" },{ false,L"Grass2" },{ false,L"Grass3" },{ false,L"Grass4" },{ false,L"Grass1" },{ false,L"Grass2" },{ false,L"Grass3" },{ false,L"Grass4" },
+									// -------  80  --------- //
 	{ true,L"Tree3" },{ true,L"Tree4" },
 
 };
